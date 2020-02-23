8c0d2cd25181451b7e24f57280bb7237823d08ad
Samuel-dot-cloud <successam336@gmail.com>
2020-02-23 23:07:35 +0300

    addition of jquery functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
-    $("img#2").click(function(){
-        $("del").toggle();
+    $("#part1").click(function() {
+        $("#desi").toggle();
+        $("#desiploy").toggle();
     });
+    $("#part2").click(function() {
+        $("#develop").toggle();
+        $("#developloy").toggle();
+    });
+    $("#part3").click(function() {
+        $("#productmn").toggle();
+        $("#productmnploy").toggle();
+    });
+});
+$().ready(function() {
+    $(".fifth").mouseenter(function() {
+        $(this).fadeTo("fast", 2)
+    })
+    $(".fifth").mouseleave(function() {
+        $(this).fadeTo("fast", 1)
+    })
+})
